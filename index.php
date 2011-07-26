c938a4a9735f3e2c3eb8b777ae74dac1c1d5f032
Zee Agency <jc@aaz.fr>
2011-07-26 13:30:38 +0200

    Better Doc

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // $isDev : Could be set by Framework, Host Detection or Whatever...
 $isDev = true;
 
-// Dev Environment : Forces New Version Generation without Minification for Debugging
+// Dev Environment : Forces Generation without Minification
 if(isset($isDev) && $isDev) {
 	include_once('cachebust/clear.php');
 }
@@ -10,7 +10,6 @@ if(isset($isDev) && $isDev) {
 // Most Important Part, Get the Current ID
 include_once('cachebust/id.php');
 // Then, you just have to put $cacheBustId before files extensions
-
 ?>
 <!doctype html>
 <html>
@@ -29,15 +28,33 @@ include_once('cachebust/id.php');
 		<li>If <var>$isDev</var> is <code>true</code> then every page refresh will generate new file versions</li>
 		<li>If <var>$isDev</var> is <code>false</code> then you must hit <code>/cachebust/clear.php</code> to force generation</li>
 		<li>For now, only CSS minification is supported. By default, this minification is only done when <var>$isDev</var> is <code>false</code>.</li>
+		<li>To force cacheburst in production environment, just hit <code>/cachebust/clear.php</code>.</li>
 	</ol>
 	
-	<h1>Example</h1>
+	<h1>PHP Code</h1>
+	
+	<pre>
+<strong>&lt;?php</strong>
+<i>// $isDev : Could be set by Framework, Host Detection or Whatever...</i>
+<var>$isDev</var> = <em>true</em>;
+
+<i>// Dev Environment : Forces Generation without Minification</i>
+<em>if</em>(<em>isset</em>(<var>$isDev</var>) && <var>$isDev</var>) {
+  <em>include_once</em>(<var>'cachebust/clear.php'</var>);
+}
+
+<i>// Most Important Part, Get the Current ID</i>
+<em>include_once</em>(<var>'cachebust/id.php'</var>);
+<i>// Then, you just have to put $cacheBustId before files extensions</i>
+<strong>?&gt;</strong>
+</pre>
+	<h1>HTML Code</h1>
 	<pre>
 &lt;!doctype html&gt;
-<em>&lt;html&gt;</em>
-  <em>&lt;link</em> href="<var>stylesheet</var>" href="<var>css/style<strong>&lt;?= $cacheBustId ?&gt;</strong>.css</var>"<em>/&gt;</em>
-  <em>&lt;script</em> src="<var>js/script<strong>&lt;?= $cacheBustId ?&gt;</strong>.js</var>"<em>&gt;&lt;/script&gt;</em>
-<em>&lt;/html&gt;</em>
+
+<em>&lt;link</em> href="<var>stylesheet</var>" href="<var>css/style<strong>&lt;?= $cacheBustId ?&gt;</strong>.css</var>"<em>/&gt;</em>
+
+<em>&lt;script</em> src="<var>js/script<strong>&lt;?= $cacheBustId ?&gt;</strong>.js</var>"<em>&gt;&lt;/script&gt;</em>
 </pre>
 	
 	
